27e9a3157dcfb9a79a34d35fa729b58060f132cc
Davinbrt <daviguedesnobertoguedes@gmail.com>
2025-05-29 14:22:16 -0300

    aula map

--- aula3/src/componentes/Lista.jsx
+++ aula3/src/componentes/Lista.jsx
@@ -4,10 +4,22 @@ export default function listarCarros(){
     return (
         <>
             <div>
+                <h1>listagem geral</h1>
                 <ul>
                     {carros.map((item, index) => (
                         <li key={index}>{item.nome} - {item.ano}</li>
                     ))}
+                </ul>
+            </div>
+            
+            <div>
+                <h1>Listagem Filtrada</h1>
+                <ul>
+                    {carros
+                        .filter((carro) => carro.ano >= 2020)
+                        .map((carro, index) => (
+                            <li key={index}>{carro.nome} -- {carro.ano}</li>
+                        ))}
                 </ul>
             </div>
         </>
