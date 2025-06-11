019dbf16a95658fbb8ce764ca21e71489fef1875
Davignbrt <161861913+Davignbrt@users.noreply.github.com>
2025-06-11 16:13:08 -0300

    Add files via upload
    estudo para a prova

--- aula3/src/componentes/Lista.jsx
+++ aula3/src/componentes/Lista.jsx
@@ -4,11 +4,42 @@ import Detalhes from "./Detalhes";
 
 export default function ListarCarros() {
     const [carroSelecionado, setCarroSelecionado] = useState(null);
+    const [anoFiltro, setAnofiltro] = useState("");
+    const carrosFiltrados = carros.filter((item) => item.ano.toString().includes(anoFiltro))
+    const [indice,setIndice] = useState(0)
+
+    function aumentar()  {
+        setIndice((indice + 1)); 
+      };
+
+    function diminuir() {
+        setIndice((indice - 1));
+    }
+    
 
     return (
         <>
-            <div className="container">
+            <div>
+                <h1>contador : {indice}</h1>
+                <button style={{ backgroundColor: indice > 10 ? "red" : "pink" }} onClick={ aumentar}>aumentar</button>
+                <button style={{ backgroundColor: indice < -10 ? "gray" : "green" }} onClick={diminuir} >diminuir</button>
+            </div>
+            <div>
+                    <input type="number"placeholder="filtrar por ano" value={anoFiltro} onChange={(e) => setAnofiltro(e.target.value)} />
+                
+            </div>
+            <div className="container_filtro">
                 {carros.map((item) => (
+                    <div className="carros" key={item.id}>
+                        <h2>{item.nome} {item.ano}</h2>
+                        <h3>Fabricante: {item.fabricante}</h3>
+                        <h4>Potência: {item.potencia} à {item.combustivel}</h4>
+                    </div>
+                ))}
+            </div>
+
+            <div className="container">
+                {carrosFiltrados.map((item) => (
                     <div className="carros" key={item.id}>
                         <h2>{item.nome} {item.ano}</h2>
                         <h3>Fabricante: {item.fabricante}</h3>
